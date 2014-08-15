835f7eb0df90eba362947cd53e83f58ad7b93b1e
Fei Tang <tfly2011@gmail.com>
2014-08-15 21:01:03 +1000

    Intial Commit

---
+++ android/src/com/diamondshearts/android/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package com.diamondshearts.android;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.diamondshearts.DiamondsHearts;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new DiamondsHearts(), config);
+	}
+}
---
+++ desktop/src/com/diamondshearts/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.diamondshearts.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.diamondshearts.DiamondsHearts;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new DiamondsHearts(), config);
+	}
+}
