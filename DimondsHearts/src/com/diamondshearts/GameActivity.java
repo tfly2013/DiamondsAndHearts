be0f958bdad1a14d7377ff576ff5a9221cefa9d4
Fei Tang <tfly2011@gmail.com>
2014-09-20 13:21:44 +1000

    create game activity

---
+++ DimondsHearts/src/com/diamondshearts/GameActivity.java
@@ -0,0 +1,19 @@
+package com.diamondshearts;
+
+import com.google.example.games.basegameutils.BaseGameActivity;
+
+public class GameActivity extends BaseGameActivity {
+
+	@Override
+	public void onSignInFailed() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void onSignInSucceeded() {
+		// TODO Auto-generated method stub
+
+	}
+
+}
